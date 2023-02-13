c100809ec25f1fb37379165a86d4edf258021478
shiroling <cintact.shiroling@gmail.com>
2023-02-13 10:25:41 +0100

    on essaie hibernate

---
+++ src/main/java/Model/Joueur.java
@@ -0,0 +1,78 @@
+package Model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "Joueur")
+public class Joueur {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "Seq_Joueur")
+    @SequenceGenerator(name = "Seq_Joueur", sequenceName = "Seq_Joueur", allocationSize = 1)
+    @Column(name = "Id_Joueur")
+    private int idJoueur;
+
+    @Column(name = "Nom", nullable = false)
+    private String nom;
+
+    @Column(name = "Prenom", nullable = false)
+    private String prenom;
+
+    @Column(name = "Date_De_Naissance", nullable = false)
+    private Date dateDeNaissance;
+
+    @Column(name = "Pseudo", nullable = false)
+    private String pseudo;
+
+    @ManyToOne
+    @JoinColumn(name = "Id_Equipe", referencedColumnName = "Id_Equipe", nullable = false)
+    private Equipe equipe;
+
+    public int getIdJoueur() {
+        return idJoueur;
+    }
+
+    public void setIdJoueur(int idJoueur) {
+        this.idJoueur = idJoueur;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public Date getDateDeNaissance() {
+        return dateDeNaissance;
+    }
+
+    public void setDateDeNaissance(Date dateDeNaissance) {
+        this.dateDeNaissance = dateDeNaissance;
+    }
+
+    public String getPseudo() {
+        return pseudo;
+    }
+
+    public void setPseudo(String pseudo) {
+        this.pseudo = pseudo;
+    }
+
+    public Equipe getEquipe() {
+        return equipe;
+    }
+
+    public void setEquipe(Equipe equipe) {
+        this.equipe = equipe;
+    }
+}
---
+++ src/main/java/Model/Tournoi.java
@@ -0,0 +1,50 @@
+package Model;
+
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Entity
+@Table(name = "Tournoi", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "nom")
+})
+public class Tournoi {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "Seq_Tournoi")
+    @SequenceGenerator(name = "Seq_Tournoi", sequenceName = "Seq_Tournoi", allocationSize = 1)
+    @Column(name = "Id_Tournoi")
+    private int id;
+
+    @Column(name = "nom", nullable = false)
+    private String name;
+
+    @Column(name = "Portee", nullable = false)
+    private String scope;
+
+    @Column(name = "dateFinInscriptions", nullable = false)
+    private Date endRegistrationDate;
+
+    @Column(name = "dateDebutTournoi", nullable = false)
+    private Date startTournamentDate;
+
+    @Column(name = "dateFinTournoi", nullable = false)
+    private Date endTournamentDate;
+
+    @ManyToOne
+    @JoinColumn(name = "Id_Jeu", nullable = false)
+    private Jeu game;
+
+    @ManyToOne
+    @JoinColumn(name = "Id_Gerant", nullable = false)
+    private Gerant manager;
+
+    // Getters and setters
+}
