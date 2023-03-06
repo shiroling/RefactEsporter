e6c35f6c2037da162a25ac10e1c005a0247b6902
Benoit <benoit.leclerc@etu.iut-tlse3.fr>
2023-03-06 12:10:07 +0100

    oue

--- src/main/java/presentation/accueil/panelCartes/TestCartes.java
+++ src/main/java/presentation/accueil/panelCartes/TestCartes.java
@@ -1,9 +1,23 @@
 package presentation.accueil.panelCartes;
 
-import javax.swing.*;
+import presentation.accueil.panelCartes.vuesCartes.CarteEquipe;
+import presentation.accueil.panelCartes.vuesCartes.CarteRencontre;
 
-public class TestCartes extends JDialog {
+import javax.swing.*;
+import java.awt.*;
+
+public class TestCartes extends JFrame {
     public TestCartes(){
+        this.setMinimumSize(new Dimension(1000,500));
+        this.setTitle("REMA");
+        this.setExtendedState(JFrame.MAXIMIZED_BOTH); // Pour l'app directement en full screen décommenter cette ligne
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        add(new CarteEquipe());
+    }
+
+    public static void main(String[] args) {
+        TestCartes test = new TestCartes();
+        test.setVisible(true);
 
     }
 }
