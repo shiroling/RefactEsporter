b9d368af19a1fe2e457abd14859f6ec3259ac136
Benoit <benoit.leclerc@etu.iut-tlse3.fr>
2023-03-09 16:01:53 +0100

    ajouter une carte est sencé fonctionner

--- src/main/java/presentation/accueil/VueAccueil.java
+++ src/main/java/presentation/accueil/VueAccueil.java
@@ -9,20 +9,28 @@ import presentation.accueil.panelCartes.vuesCartes.Carte;
 import presentation.accueil.panelFonctionnalite.VueFonctionalite;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 public class VueAccueil extends JFrame {
     private final VueFonctionalite vueFonctionalite;
     private final VuePanelCarte vueCarte;
     public VueAccueil() {
-        this.vueFonctionalite = new VueFonctionalite();
-        this.vueCarte = new VuePanelCarte();
         this.setMinimumSize(new Dimension(1000,500));
         this.setTitle("REMA");
         this.setExtendedState(JFrame.MAXIMIZED_BOTH); // Pour l'app directement en full screen décommenter cette ligne
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        this.vueFonctionalite = new VueFonctionalite();
+        this.vueCarte = new VuePanelCarte();
+        JScrollPane scrollPaneMain = new JScrollPane();
+
+        scrollPaneMain.setBorder(new EmptyBorder(0,0,0,0));
+        scrollPaneMain.getVerticalScrollBar().setUnitIncrement(20);
+
+        scrollPaneMain.setViewportView(vueCarte);
         this.getContentPane().add(vueFonctionalite, BorderLayout.WEST);
-        this.getContentPane().add(vueCarte, BorderLayout.CENTER);
+        this.getContentPane().add(scrollPaneMain, BorderLayout.CENTER);
     }
 
     public VueFonctionalite getVueFonctionalite() {
